a800064ced928e0230f416670e72d2607da9fcad
pilyaev_av <pilyaev_av>
2023-04-27 18:00:16 +0300

    select city

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,41 @@
 import * as React from 'react';
-import { useTheme } from '@mui/material/styles';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
+import Button from '@mui/material/Button';
+import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import FormControl from '@mui/material/FormControl';
-import Select from '@mui/material/Select';
 import logo from '../img/logo.png';
+import { styled } from '@mui/material/styles';
 
-const ITEM_HEIGHT = 48;
-const ITEM_PADDING_TOP = 8;
-const MenuProps = {
-  PaperProps: {
-    style: {
-      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-      width: 250,
-    },
-  },
-};
-
-const names = [
-  'Oliver Hansen',
-  'Van Henry',
-  'April Tucker',
-  'Ralph Hubbard',
-  'Omar Alexander',
-  'Carlos Abbott',
-  'Miriam Wagner',
-  'Bradley Wilkerson',
-  'Virginia Andrews',
-  'Kelly Snyder',
+const options = [
+  'Ростов-на-Дону',
+  'Таганрог',
+  'Шахты',
+  'Азов',
+  'Сальск',
+  'Элиста',
+  'Краснодар',
 ];
 
-function getStyles(name, personName, theme) {
-  return {
-    fontWeight:
-      personName.indexOf(name) === -1
-        ? theme.typography.fontWeightRegular
-        : theme.typography.fontWeightMedium,
-  };
-}
+const CustomButton = styled(Button)(({ theme }) => ({
+  background: 'white'
+}));
 
+const ITEM_HEIGHT = 48;
 
 function Header() {
-  const theme = useTheme();
-  const [personName, setPersonName] = React.useState([]);
-
-  const handleChange = (event) => {
-    console.log(23)
-    const {
-      target: { value },
-    } = event;
-    setPersonName(
-      // On autofill we get a stringified value.
-      typeof value === 'string' ? value.split(',') : value,
-    );
+  const [btnPlaceholder, setBtnPlaceholder] = React.useState('Выбрать город');
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
   };
 
+  const handleSelect = (e) => {
+    setBtnPlaceholder(e.target.textContent)
+    setAnchorEl(null);
+  };
   return (
      <header>
       <div className='logo'>
@@ -65,28 +45,37 @@ function Header() {
         </div>
       </div>
       <div className='select-city'>
-      <FormControl sx={{ m: 1, width: 300 }}>
-        <InputLabel  id="demo-multiple-name-label">Name</InputLabel>
-        <Select
-          labelId="demo-multiple-name-label"
-          id="demo-multiple-name"
-          multiple
-          value={personName}
-          onChange={handleChange}
-          input={<OutlinedInput label="Name" />}
-          MenuProps={MenuProps}
-        >
-          {names.map((name) => (
-            <MenuItem
-              key={name}
-              value={name}
-              style={getStyles(name, personName, theme)}
-            >
-              {name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+      <Button variant="contained" style={{
+            background: 'white',
+            color: '#3574F2',
+            width: '153.172px',
+            fontSize: '12px',
+            fontWeight: '700'
+          }}
+        onClick={handleClick}>
+            { btnPlaceholder }
+          </Button>
+      <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button',
+        }}
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        PaperProps={{
+          style: {
+            maxHeight: ITEM_HEIGHT * 4.5,
+            width: '20ch',
+          },
+        }}
+      >
+        {options.map((option) => (
+          <MenuItem key={option} selected={option === 'Pyxis'} onClick={handleSelect}>
+            {option}
+          </MenuItem>
+        ))}
+      </Menu>
     </div>
       <div></div>
      </header>
