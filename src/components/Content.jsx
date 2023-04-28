45073370b96455c8cee410057df30d6d30f07997
pilyaev_av <pilyaev_av>
2023-04-28 18:49:08 +0300

    список с автобусами и их метками

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,27 +1,211 @@
 import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import MuiAccordion from '@mui/material/Accordion';
+import MuiAccordionSummary from '@mui/material/AccordionSummary';
+import MuiAccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+
+const Accordion = styled((props) => (
+  <MuiAccordion elevation={0} square {...props} />
+))(({ theme }) => ({
+  border: `1px solid ${theme.palette.divider}`,
+  '&:before': {
+    display: 'none',
+  },
+}));
+
+const AccordionSummary = styled((props) => (
+  <MuiAccordionSummary
+    expandIcon={<ArrowForwardIosSharpIcon sx={{ fontSize: '0.9rem' }} />}
+    {...props}
+  />
+))(({ theme }) => ({
+  backgroundColor:
+    theme.palette.mode === 'dark'
+      ? 'rgba(255, 255, 255, .05)'
+      : 'rgba(0, 0, 0, .03)',
+  flexDirection: 'row-reverse',
+  '& .MuiAccordionSummary-expandIconWrapper.Mui-expanded': {
+    transform: 'rotate(90deg)',
+  },
+  '& .MuiAccordionSummary-content': {
+    marginLeft: theme.spacing(1),
+  },
+}));
+
+const AccordionDetails = styled(MuiAccordionDetails)(({ theme }) => ({
+  borderTop: '1px solid rgba(0, 0, 0, .125)',
+  padding: '0px'
+}));
 
 function Content() {
-  return (
+  const [expanded, setExpanded] = React.useState('panel1');
+  const handleChange = (panel) => {
+    setExpanded(panel == expanded ? '' : panel);
+  };
+  const [data, setData] = React.useState([])
+  React.useEffect(() => {
+  if (!window.first_render) {
+      fetch('///').then((res) => {
+      }).catch(() => {
+          const buses = {}
+          const res = [
+            {
+               id: 97123557,
+               transport_id: "2260",
+               lat: 47226287,
+               lon: 39737388,
+               speed: 45,
+               angle: 75,
+               created_at: "2022-01-10 08:00:02"
+            },
+            {
+               id: 97123557,
+               transport_id: "2260",
+               lat: 47226287,
+               lon: 39737388,
+               speed: 44,
+               angle: 74,
+               created_at: "2022-01-10 07:00:02"
+            },
+            {
+               id: 97123557,
+               transport_id: "2261",
+               lat: 47226287,
+               lon: 39737388,
+               speed: 44,
+               angle: 74,
+               created_at: "2022-01-10 07:00:02"
+            },
+            {
+               id: 97123557,
+               transport_id: "2262",
+               lat: 47226287,
+               lon: 39737388,
+               speed: 44,
+               angle: 74,
+               created_at: "2022-01-10 07:00:02"
+            },
+            {
+               id: 97123557,
+               transport_id: "2260",
+               lat: 47226287,
+               lon: 39737388,
+               speed: 43,
+               angle: 73,
+               created_at: "2022-01-10 09:00:02"
+            },
+            {
+               id: 97123557,
+               transport_id: "2260",
+               lat: 47226287,
+               lon: 39737388,
+               speed: 42,
+               angle: 72,
+               created_at: "2022-01-10 07:10:02"
+            },
+            {
+               id: 97123557,
+              transport_id: "2260",
+              lat: 47226287,
+              lon: 39737388,
+              speed: 41,
+              angle: 71,
+              created_at: "2022-01-10 07:05:02"
+            },
+          ]
+          res.map( el => {
+              if (!Object.keys(buses).includes(el.transport_id)) {
+                  buses[el.transport_id] = {
+                      id: el.transport_id,
+                      open: false,
+                      marks: [
+                        {
+                          angle: el.angle,
+                          speed: el.speed,
+                          lat: el.lat,
+                          lon: el.lon,
+                          time: el.created_at
+                        }
+                      ] 
+                  }
+              } else {
+                buses[el.transport_id].marks.push({
+                  angle: el.angle,
+                  speed: el.speed,
+                  lat: el.lat,
+                  lon: el.lon,
+                  time: el.created_at
+                })
+              }
+          })
+        Object.keys(buses).map( el => {
+          buses[el].marks = buses[el].marks.sort(function(a,b){
+            return new Date(b.time) - new Date(a.time);
+          });
+        })
+        setData(buses)
+      })
+  } else {
+      window.first_render = true;
+  }
+  },[])
+
+
+    function displayBuses () {
+        return Object.keys(data).map(el => {
+            return  <Accordion expanded={expanded === data[el].id}>
+            <AccordionSummary onClick={()=>handleChange(data[el].id)} aria-controls="panel1d-content" id="panel1d-header">
+              <div className='bus-info'>
+                <Typography title={`Автобус №${data[el].id}`}>Автобус №{data[el].id}</Typography>
+                <Typography title={`${data[el].marks[0].speed} км/ч`}>{data[el].marks[0].speed} км/ч</Typography>
+                <Typography title={`${data[el].marks[0].angle}°`}>{data[el].marks[0].angle}°</Typography>
+                <Typography title={`${data[el].marks[0].lat} / ${data[el].marks[0].lon}`}>{data[el].marks[0].lat} / {data[el].marks[0].lon}</Typography>
+                <Typography title={`${data[el].marks[0].time}`}>{data[el].marks[0].time}</Typography>
+              </div>
+            </AccordionSummary>
+            <AccordionDetails>
+              <List>
+                {(() => {
+                  return [
+                    <ListItem disablePadding >
+                    <ListItemButton>
+                      <div className='mark mark-labels'>
+                        <ListItemText primary='км/ч' />
+                        <ListItemText primary='направление' />
+                        <ListItemText primary='координаты' />
+                        <ListItemText primary='время' />
+                      </div>
+                    </ListItemButton>
+                  </ListItem>,
+                  ...data[el].marks.map( mark => {
+                    return <ListItem disablePadding >
+                    <ListItemButton>
+                      <div className='mark'>
+                        <ListItemText primary={mark.speed} />
+                        <ListItemText primary={mark.angle} />
+                        <ListItemText primary={`${mark.lat} / ${mark.lon}`} />
+                        <ListItemText primary={mark.time} />
+                      </div>
+                    </ListItemButton>
+                  </ListItem>
+                  })]
+                })()}
+                
+              </List>
+            </AccordionDetails>
+          </Accordion>
+        })
+    }
+    return (
      <body>
         <div className="contentWrapper">
-            <details>
-                <summary className="detailsHeading">
-                    <span>Название автобуса</span>
-                    <img src="../img/expand.svg" className="expand"/>
-                </summary>
-                <div className="detailsInfo">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu odio eget lectus tincidunt maximus at a dolor. Etiam gravida, tellus sit amet scelerisque aliquet, mauris lacus condimentum dolor, sed ultrices ipsum diam nec nulla. Sed tincidunt faucibus elit, eget lobortis felis facilisis sit amet. Suspendisse blandit vestibulum scelerisque. Fusce feugiat augue urna, a convallis arcu efficitur quis. Nunc non pulvinar augue, ut lobortis dui. Morbi imperdiet magna nibh, non accumsan magna tincidunt lobortis. Nunc dignissim dolor vitae neque tincidunt iaculis.
-                </div>
-            </details>
-            <details>
-                <summary className="detailsHeading">
-                    <span>Название автобуса</span>
-                    <img src="../img/expand.svg" className="expand"/>
-                </summary>
-                <div className="detailsInfo">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eu odio eget lectus tincidunt maximus at a dolor. Etiam gravida, tellus sit amet scelerisque aliquet, mauris lacus condimentum dolor, sed ultrices ipsum diam nec nulla. Sed tincidunt faucibus elit, eget lobortis felis facilisis sit amet. Suspendisse blandit vestibulum scelerisque. Fusce feugiat augue urna, a convallis arcu efficitur quis. Nunc non pulvinar augue, ut lobortis dui. Morbi imperdiet magna nibh, non accumsan magna tincidunt lobortis. Nunc dignissim dolor vitae neque tincidunt iaculis.
-                </div>
-            </details>
+            { displayBuses() }
         </div>
 
      </body>
